fc996a0d0d78bb8325aa7df8e1d9c67a527898c7
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-29 15:15:02 -0500

    Added list query to service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,3 +1,10 @@
 const db = require("../db/connection");
+const tableName = "reservations";
+/**
+ * List query fetches all of the table data as an array
+ */
+function list() {
+  return db.tableName.select("*");
+}
 
-module.exports = {};
+module.exports = { list };
