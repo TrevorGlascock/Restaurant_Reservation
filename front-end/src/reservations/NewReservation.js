f81b29f189d8f52d1fe8e6059f96f129a4347826
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-29 10:53:41 -0500

    Made rudimentary NewReservation Component, still needs to be a proper form

---
+++ front-end/src/reservations/NewReservation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NewReservation() {
+  return (
+    <main>
+      <h1>New Reservation</h1>
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0">Reservations for date</h4>
+      </div>
+    </main>
+  );
+}
