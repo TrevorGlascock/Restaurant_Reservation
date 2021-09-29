2395844ecc195ddc05b2a63fbeeb6865c9cc20f1
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-29 15:44:59 -0500

    Added create query to service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -7,4 +7,15 @@ function list() {
   return db.tableName.select("*");
 }
 
+/**
+ * Create inserts a new Reservation into the table data
+ * and returns the inserted object
+ */
+function create(NewReservation) {
+  return db.tableName
+    .insert(NewReservation)
+    .returning("*")
+    .then((rows) => rows[0]);
+}
+
 module.exports = { list };
