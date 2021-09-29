d4bfac0599e509c5a980bce2afdc435963c0d319
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-29 11:50:32 -0500

    Added form fields for firstname, lastname, and moblenumber

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -3,9 +3,50 @@ import React from "react";
 export default function NewReservation() {
   return (
     <main>
-      <h1>New Reservation</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <form>
+          <fieldset>
+            <legend className="h1">New Reservation</legend>
+            <div className="form-group">
+              <label for="first-name">First Name</label>
+              <input
+                id="first_name"
+                type="text"
+                name="first_name"
+                placeholder="Enter your first name"
+                className="form-control"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label for="last-name">Last Name</label>
+              <input
+                id="last_name"
+                type="text"
+                name="last_name"
+                placeholder="Enter your last name"
+                className="form-control"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label for="mobile_number">Mobile number</label>
+              <input
+                id="mobile_number"
+                type="text"
+                name="mobile_number"
+                placeholder="Enter your mobile phone number"
+                className="form-control"
+                required
+              />
+            </div>
+
+            <button type="submit">Submit</button>
+            <button type="button">Cancel</button>
+          </fieldset>
+        </form>
       </div>
     </main>
   );
