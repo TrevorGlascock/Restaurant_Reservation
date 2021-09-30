a659621eb2da49845807f9796343828a8281516a
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-30 14:10:57 -0500

    Incorporated the TableHead component into the DisplayTable

--- front-end/src/dashboard/DisplayTable.js
+++ front-end/src/dashboard/DisplayTable.js
@@ -1,14 +1,19 @@
 import React from "react";
+import TableHead from "./TableHead";
 
 /**
  * Defines the front-end table displaying appropriate data.
  * @param data
  *  the current array of objects to display in table
  * @param type
- *  a string describing the type of object being rendered in each row, 
+ *  a string describing the type of object being rendered in each row,
  *  this allow us to choose the type of row component to use
  * @returns {JSX.Element}
  */
 export default function DisplayTable({ data, type }) {
-  return <table></table>;
+  return (
+    <table className="table">
+      <TableHead type={type} />
+    </table>
+  );
 }
