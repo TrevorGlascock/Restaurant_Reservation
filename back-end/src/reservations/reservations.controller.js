cabe3adaba7dc48ceec989258ab8f25e6912d3ee
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-30 10:36:51 -0500

    added VALID_PROPERTIES key array, validateReservation middleware, and utilized res.locals to store the reservation obj on the response

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,6 +1,34 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./reservations.service");
 
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
+
+/**
+ * Middleware validation for request bodies to ensure it is a proper reservation
+ */
+function validateReservation(req, res, next) {
+  const { data: reservation = {} } = req.body;
+  const invalidFields = Object.keys(reservation).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  res.locals.reservation = reservation;
+  return next();
+}
+
 /**
  * List handler for reservation resources
  */
@@ -13,11 +41,11 @@ async function list(req, res) {
  * Create handler for new Reservations
  */
 async function create(req, res) {
-  await service.create(req.body.data);
+  await service.create(res.locals.reservation);
   res.sendStatus(201);
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: asyncErrorBoundary(create),
+  create: [validateReservation, asyncErrorBoundary(create)],
 };
