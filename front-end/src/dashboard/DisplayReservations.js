e370a490d566452df5e2049c80b5637994f16f2e
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-30 12:46:22 -0500

    Defined the DisplayReservations component

---
+++ front-end/src/dashboard/DisplayReservations.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * Defines the front-end table displaying appropriate reservations.
+ * @param reservations
+ *  the current array of reservation objects to display
+ * @returns {JSX.Element}
+ */
+export default function DisplayReservations({ reservations }) {
+  return <table></table>;
+}
