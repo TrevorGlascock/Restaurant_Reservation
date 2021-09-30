d4e5d54ff803d7ff6d5881440b21bc6ffe2966ed
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-30 13:14:37 -0500

    Renamed DisplayReservations component to DisplayTable, and recontextualized it as a dynamic table rendering component

---
+++ front-end/src/dashboard/DisplayTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Defines the front-end table displaying appropriate data.
+ * @param data
+ *  the current array of objects to display in table
+ * @param type
+ *  a string describing the type of object being rendered in each row, 
+ *  this allow us to choose the type of row component to use
+ * @returns {JSX.Element}
+ */
+export default function DisplayTable({ data, type }) {
+  return <table></table>;
+}
