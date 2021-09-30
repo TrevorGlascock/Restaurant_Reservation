e437ce7a8137e90c3a1bb04bbff638dc3d5e8102
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-30 15:01:53 -0500

    map through the data passed in to generate the rows of the table

--- front-end/src/dashboard/DisplayTable.js
+++ front-end/src/dashboard/DisplayTable.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TableHead from "./TableHead";
+import TableRow from "./TableRow";
 
 /**
  * Defines the front-end table displaying appropriate data.
@@ -11,9 +12,14 @@ import TableHead from "./TableHead";
  * @returns {JSX.Element}
  */
 export default function DisplayTable({ data, type }) {
+  const rows = data?.map((object, index) => (
+    <TableRow data={object} key={index} />
+  ));
+
   return (
     <table className="table">
       <TableHead type={type} />
+      {rows}
     </table>
   );
 }
