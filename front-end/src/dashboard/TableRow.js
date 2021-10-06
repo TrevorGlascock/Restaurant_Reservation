8dc1e04730b54da8574d08121f0e9e7077a1a53d
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 09:28:27 -0500

    Added support in the dynamic table for boolean data values

--- front-end/src/dashboard/TableRow.js
+++ front-end/src/dashboard/TableRow.js
@@ -6,7 +6,7 @@ import React from "react";
  *  An object that contains some of the properties that make up the table's row
  * @param propNames
  *  An array of all the property names in this rowObject that make up the table's row
- * @var row 
+ * @var row
  *  An array of JSX table-data containing all the data of that makes up this row
  * @returns {JSX.Element}
  */
@@ -14,7 +14,13 @@ export default function TableRow({ rowObject, propNames }) {
   const row = [];
   for (let index in propNames) {
     const propName = propNames[index];
-    row.push(<td key={index}>{rowObject[propName]}</td>);
+    const data = rowObject[propName];
+
+    // if data is a boolean, we will need to disply a status string based on the boolean value
+    const isBoolean = typeof data === "boolean";
+    const status = data ? "Occupied" : "Open";
+
+    row.push(<td key={index}>{isBoolean ? status : data}</td>);
   }
   return <tr>{row}</tr>;
 }
