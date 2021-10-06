663a6123e77830066d1f52466d33c00c28c676c8
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 13:56:40 -0500

    Created basic SeatReservation component for testing purposes

---
+++ front-end/src/reservations/SeatReservation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Defines the Reservation-Table Assignment Page.
+ * @returns {JSX.Element}
+ */
+export default function SeatReservation() {
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <div className="d-md-flex mb-3"></div>
+      <h4 className="h4">Assign a table for reservation #</h4>
+    </main>
+  );
+}
