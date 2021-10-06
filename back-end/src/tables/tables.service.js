b35d8e1986ad98daf7b0dad9ee6992af1e824241
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 09:06:04 -0500

    Added list knex Query to tables.service

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,4 +1,10 @@
 const db = require("../db/connection");
 const tableName = "tables";
+/**
+ * List query fetches all of the table data sorted by table_name
+ */
+function list() {
+  return db(tableName).select("*").orderBy("table_name");
+}
 
-module.exports = {};
+module.exports = { list };
