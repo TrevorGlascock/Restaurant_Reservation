d5a3f1bef381657922ae44a47808014ab9edd563
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 09:46:49 -0500

    Replaced button with a Link styled to look like a button

--- front-end/src/dashboard/SeatButton.js
+++ front-end/src/dashboard/SeatButton.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 /**
  * Defines a seat button element to dynamically assign a reservation to a table
  * @returns {JSX.Element}
  */
-const onClick = () => {
-  console.log("Seating Button!");
-};
+
 export default function SeatButton() {
-  return <button onClick={onClick}>Seat</button>;
+  return (
+    <Link className="btn btn-primary" to="test">
+      Seat
+    </Link>
+  );
 }
