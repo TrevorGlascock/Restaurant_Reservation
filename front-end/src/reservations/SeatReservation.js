8110b1ab46186ba55b2944d59a28342e1862a339
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 14:04:04 -0500

    Added useParams to extract the reservationId

--- front-end/src/reservations/SeatReservation.js
+++ front-end/src/reservations/SeatReservation.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { useParams } from "react-router";
 
 /**
  * Defines the Reservation-Table Assignment Page.
  * @returns {JSX.Element}
  */
 export default function SeatReservation() {
+  const { reservationId } = useParams();
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3"></div>
-      <h4 className="h4">Assign a table for reservation #</h4>
+      <h4 className="h4">Assign a table for reservation #{reservationId}</h4>
     </main>
   );
 }
