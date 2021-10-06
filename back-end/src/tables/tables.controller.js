acd5e08e4e9c596bd14565693cb36c0cb25827fa
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 09:14:01 -0500

    Fixed incorrect import

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,5 +1,5 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-const service = require("./reservations.service");
+const service = require("./tables.service");
 
 /**
  * List handler for tables resource
