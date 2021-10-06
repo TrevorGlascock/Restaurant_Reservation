1a95cb94ca51bcd2307601cb09c3f36d7a20d254
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-06 08:54:05 -0500

    Added list handler function to tables.controller

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,4 +1,13 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./reservations.service");
 
-module.exports = {};
+/**
+ * List handler for tables resource
+ */
+async function list(req, res) {
+  const { date } = req.query;
+  const data = await service.list(date);
+  res.json({ data });
+}
+
+module.exports = { list: asyncErrorBoundary(list) };
