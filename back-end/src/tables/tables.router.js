faba59cdcf022deac331d320bd20dc066c9aceb4
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-07 17:44:28 -0500

    Implemented methodNotAllowed function for all methods that are not currently supported

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -6,9 +6,17 @@
 
 const router = require("express").Router();
 const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).post(controller.create);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
-router.route("/:table_id").put(controller.assignReservation);
+router
+  .route("/:table_id")
+  .put(controller.assignReservation)
+  .all(methodNotAllowed);
 
 module.exports = router;
