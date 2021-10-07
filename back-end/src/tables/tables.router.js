f01b3d3b5ce9bb04525a6ec315ffb422bc41839a
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-07 17:39:39 -0500

    Route put requests to '/tables/:table_id'

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -9,4 +9,6 @@ const controller = require("./tables.controller");
 
 router.route("/").get(controller.list).post(controller.create);
 
+router.route("/:table_id").put(controller.assignReservation);
+
 module.exports = router;
