f6335f904aa0b9c8a01497fcf2eb8f1a6f62bdff
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-10 10:27:18 -0500

    Created FinishButton component, which will free up an occupied table once functionality is added

---
+++ front-end/src/dashboard/FinishButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * Defines a finish button element to free up an occupied table
+ * @returns {JSX.Element}
+ */
+
+export default function FinishButton({ id }) {
+  return <button className="btn btn-danger">Finish</button>;
+}
