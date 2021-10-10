21ab4e841347858099551f9527d12c1bee559934
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-10 10:27:54 -0500

    Created AssignmentButton component to conditonally render the Seat/Finish Buttons

---
+++ front-end/src/dashboard/AssignmentButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import FinishButton from "./FinishButton";
+import SeatButton from "./SeatButton";
+
+/**
+ * This component will conditionally render either a SeatButton or a FinishButton
+ * @param rowObject
+ *  the parent object will either be a reservation or a table, which will define the condition for the render
+ * @param table_id
+ *  table objects will have a table_id primary key
+ * @param reservation_id
+ *  reservation objects will have reservation_id primary key
+ *  table objects will have a (possibly null) reservation_id foreign key
+ * @returns {JSX.Element}
+ */
+
+export default function AssignmentButton({
+  rowObject: { table_id = null, reservation_id = null },
+}) {
+  return table_id ? (
+    // Table objects have FinishButtons
+    <FinishButton id={table_id} />
+  ) : (
+    // Reservation objects have SeatButtons
+    <SeatButton id={reservation_id} />
+  );
+}
