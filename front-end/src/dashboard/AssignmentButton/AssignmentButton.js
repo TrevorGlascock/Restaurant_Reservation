223e2b1222768bdad061cec4f44c8833f7375bd3
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-11 10:17:13 -0500

    Refactoed AssignmentButton component to pass down the entire rowObject to the children components

--- front-end/src/dashboard/AssignmentButton/AssignmentButton.js
+++ front-end/src/dashboard/AssignmentButton/AssignmentButton.js
@@ -5,24 +5,22 @@ import SeatButton from "./SeatButton";
 /**
  * This component will conditionally render either a SeatButton or a FinishButton
  * @param rowObject
- *  the parent object will either be a reservation or a table, which will define the condition for the render
- * @param table_id
+ *  the entire object passed in from the parent component
+ *  will either be a table object or a reservation object
+ * @var table_id
+ *  destructed from rowObject param to distinguish between the types of objects
  *  table objects will have a table_id primary key
- * @param reservation_id
- *  reservation objects will have reservation_id primary key
- *  table objects will have a (possibly null) reservation_id foreign key
+ *  reservation objects will have a null table_id
  * @returns {JSX.Element}
  */
 
-export default function AssignmentButton({
-  rowObject: { table_id = null, reservation_id = null },
-  finishTable,
-}) {
+export default function AssignmentButton({ rowObject, finishTable }) {
+  const { table_id = null } = rowObject;
   return table_id ? (
     // Table objects have FinishButtons
-    <FinishButton id={table_id} finishTable={finishTable} />
+    <FinishButton table={rowObject} finishTable={finishTable} />
   ) : (
     // Reservation objects have SeatButtons
-    <SeatButton id={reservation_id} />
+    <SeatButton reservation={rowObject} />
   );
 }
