7a6541c0b35f2a0dd161f3d76debfa0331827387
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-11 18:41:19 -0500

    Slight change to list function to order the full list of reservations by their id, to better aid in the debug process

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,7 +4,8 @@ const tableName = "reservations";
  * List query fetches all of the table data where reservation_date equals the passed in param
  */
 function list(reservation_date) {
-  if (!reservation_date) return db(tableName).select("*");
+  if (!reservation_date)
+    return db(tableName).select("*").orderBy("reservation_id", "ASC");
   return db(tableName)
     .select("*")
     .orderBy("reservation_time", "ASC")
