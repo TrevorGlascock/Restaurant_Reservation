12fbb12a891e139310725081c06ab7e2ea2ecf96
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-11 10:26:12 -0500

    Improved comment documentation

--- front-end/src/dashboard/AssignmentButton/SeatButton.js
+++ front-end/src/dashboard/AssignmentButton/SeatButton.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 
 /**
  * Defines a seat button element to dynamically assign a reservation to a table
+ * @param reservation
+ *  the reservation object this button corresponds to
+ * @var id
+ *  the reservation_id destructed from the reservation object
  * @returns {JSX.Element}
  */
 
