c2ca1905aadb9e4046ee6a904f5aff32b25b1126
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-12 15:27:14 -0500

    Gave the Search component a searchQuery useState variable and some barebones functionality for testing purposes

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 /**
  * Defines the Search page.
  * @returns {JSX.Element}
  */
 export function Search() {
+  const [searchQuery, setSearchQuery] = useState(""); // useState control form that defines the search query for the API call
+  const [errorsArray, setErrorsArray] = useState([]); // All errors in validation, potential API error on submit, and finally the tablesError, if it isn't null
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // prevents the submit button's default behavior
+    setErrorsArray([]);
+    console.log(searchQuery);
+  };
+
   return (
     <main>
-      <h1>Search for Reservations by Phone number</h1>
-      <div className="d-md-flex mb-3"></div>
+      <div className="d-md-flex mb-3">
+        <form onSubmit={submitHandler}>
+          <fieldset>
+            <legend className="h1">Search for Reservations</legend>
+            <div className="form-group">
+              <label htmlFor="mobile_number">Mobile Number</label>
+              <input
+                id="mobile_number"
+                type="text"
+                name="mobile_number"
+                placeholder="Enter a customer's phone number"
+                title="Enter a customer's phone number"
+                className="form-control"
+                value={searchQuery}
+                onChange={({ target }) => setSearchQuery(target.value)}
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary ">
+              Find
+            </button>
+          </fieldset>
+        </form>
+      </div>
     </main>
   );
 }
