ef7854eede2dc369d3d67ad0b29fc5a865f9e430
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-12 14:10:42 -0500

    Created new 'search' directory and created Search component

---
+++ front-end/src/search/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Defines the Search page.
+ * @returns {JSX.Element}
+ */
+export function Search() {
+  return (
+    <main>
+      <h1>Search for Reservations by Phone number</h1>
+      <div className="d-md-flex mb-3"></div>
+    </main>
+  );
+}
