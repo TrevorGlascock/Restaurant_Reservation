ba6ac101f89a2091176a7642feb5634143c98b00
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-15 14:00:23 -0500

    Restyled component to always be a column

--- front-end/src/reservations/DisplayReservation.js
+++ front-end/src/reservations/DisplayReservation.js
@@ -20,44 +20,42 @@ export default function DisplayReservation({ reservation }) {
   const time = convert12HourTime(reservation.reservation_time);
 
   return (
-    <div>
-      <aside>
-        <table className="table table-secondary table-hover">
-          <thead>
-            <th>Reservation #{reservation_id} Info</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>First Name</td>
-              <td>{first_name}</td>
-            </tr>
-            <tr>
-              <td>Last Name</td>
-              <td>{last_name}</td>
-            </tr>
-            <tr>
-              <td>Mobile Phone</td>
-              <td>{mobile_number}</td>
-            </tr>
-            <tr>
-              <td>Date of Reservation</td>
-              <td>{date}</td>
-            </tr>
-            <tr>
-              <td>Time of Reservation</td>
-              <td>{time}</td>
-            </tr>
-            <tr>
-              <td>Size of Party</td>
-              <td>{people}</td>
-            </tr>
-            <tr>
-              <td>Status of Reservation</td>
-              <td>{status}</td>
-            </tr>
-          </tbody>
-        </table>
-      </aside>
+    <div className="col">
+      <table className="table table-secondary table-hover">
+        <thead>
+          <th>Reservation #{reservation_id} Info</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>First Name</td>
+            <td>{first_name}</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>{last_name}</td>
+          </tr>
+          <tr>
+            <td>Mobile Phone</td>
+            <td>{mobile_number}</td>
+          </tr>
+          <tr>
+            <td>Date of Reservation</td>
+            <td>{date}</td>
+          </tr>
+          <tr>
+            <td>Time of Reservation</td>
+            <td>{time}</td>
+          </tr>
+          <tr>
+            <td>Size of Party</td>
+            <td>{people}</td>
+          </tr>
+          <tr>
+            <td>Status of Reservation</td>
+            <td>{status}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
