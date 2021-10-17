6f3c239f9dced34d0413e2d6482fcca7aeea29f2
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-10-16 20:21:55 -0500

    Implemented LoadingPrompt Component to have dashboard become more user friendly while waiting for API calls

--- front-end/src/loading/LoadingPrompt.js
+++ front-end/src/loading/LoadingPrompt.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { usePromiseTracker } from "react-promise-tracker";
 
-export default function LoadingPrompt() {
+/**
+ * Defines a Loading Prompt Component to make the delay in asynchronous API-Calls more user-friendly
+ * @param alternateComponent
+ *  a JSX element to be rendered when the promiseInProgress is completed.
+ * @returns
+ */
+export default function LoadingPrompt({ component = null }) {
   const { promiseInProgress } = usePromiseTracker();
-  return promiseInProgress && <h1>Now Loading...</h1>;
+  return promiseInProgress ? (
+    <h1 className="text-center mt-5">Now Loading...</h1>
+  ) : (
+    component
+  );
 }
